ba39408c672b30cafc3a9f0887b46906fb360988
Emon Hossain <emondd4@gmail.com>
2021-11-05 09:54:37 +0600

    ktor init project

---
+++ ktor-demo/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "192.168.0.109") {
+        configureRouting()
+    }.start(wait = true)
+}
---
+++ ktor-demo/src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,22 @@
+package com.example.plugins
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello World")
+        }
+    }
+
+    routing {
+        get("/emon") {
+            call.respondText("Hello Emon Hossain Munna!")
+        }
+    }
+}
